88de6b279476213cb36045dcb5df338052b1479b
nimm2sugus <126090840+nimm2sugus@users.noreply.github.com>
2025-04-25 10:01:25 +0200

    Update StromeinkaufsanalyseApp.py

--- StromeinkaufsanalyseApp.py
+++ StromeinkaufsanalyseApp.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 
 def load_excel_file(uploaded_file):
     try:
@@ -19,12 +20,22 @@ if uploaded_file is not None:
     df = load_excel_file(uploaded_file)
 
     if df is not None:
-        st.subheader("Originaldaten")
-        df['Beendet'] = pd.to_datetime(df['Beendet'], errors='coerce')
-        df['Verbrauch [kWh]'] = pd.to_numeric(df['Verbrauch [kWh]'], errors='coerce')
-
-        df['Monat'] = df['Beendet'].dt.month
-        df['Tag'] = df['Beendet'].dt.day
-        df['Stunde'] = df['Beendet'].dt.hour
-
         st.write(df)
+    else:
+        st.warning("Dies ist ein komisches Format für den Upload :/")
+
+        # Verbrauch im Zeitverlauf (alle Einzelvorgänge)
+        st.subheader("Normierte Verläufe")
+        fig_line_verbrauch = px.line(df, x="Beendet", y="Verbrauch normiert", title="Verbrauch normiert", markers=True)
+        fig_line_marktpreis = px.line(df, x="Zeitstempel Day Ahead Marktpreis", y="Day Ahead Marktpreis normiert", title="Day Ahead Marktpreis normiert", markers=True)
+
+        # Neue Figure erstellen und Traces aus beiden px-Figuren übernehmen
+        fig_combined = go.Figure()
+
+        # Traces von fig1 und fig2 hinzufügen
+        for trace in fig_line_verbrauch.data:
+            fig_combined.add_trace(trace)
+        for trace in fig_line_marktpreis.data:
+            fig_combined.add_trace(trace)
+
+        st.plotly_chart(fig_combined, use_container_width=True)
